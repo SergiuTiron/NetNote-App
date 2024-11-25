b40c51e6268bf8afdfc879c753515d351ffe4656
Melle Moerkerk <m.c.moerkerk@student.tudelft.nl>
2024-11-25 18:12:45 +0100

    Implement basic adding/editing of notes
    - Basically renamed quotes -> notes
    - Implemented basic GUI with add note, refresh & save changes buttons
    - Removed all references to quotes & people

---
+++ commons/src/main/java/commons/Note.java
@@ -0,0 +1,40 @@
+package commons;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    public String content;
+
+    public Note() {
+
+    }
+
+    public Note(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return id == note.id && Objects.equals(content, note.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content);
+    }
+
+}
